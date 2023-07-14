a9bd5ec5de06f4e5dd91184403b9e560335ffad5
Aidan Pine <hello@aidanpine.ca>
2023-07-13 17:31:53 -0700

    --wip-- [skip ci]

--- packages/mtd-mobile-ui/src/app/search/search.page.ts
+++ packages/mtd-mobile-ui/src/app/search/search.page.ts
@@ -19,6 +19,11 @@ export class SearchPage implements OnInit {
   ngOnInit(): void {
   }
 
+  linearSearch(query:string){
+    const regex = new RegExp(query)
+    return ENTRIES.filter((word) => word.word.match(regex) || word.definition.match(regex))
+  }
+
   search(ev: Event) {
     if (ev.target === null) {
       return
@@ -27,10 +32,15 @@ export class SearchPage implements OnInit {
     const query = (ev.target as HTMLTextAreaElement).value
     this.searchQuery = query;
     if (query.length > 1) {
-      const t0 = Date.now();
+      let t0 = Date.now();
       const results = this.searchService.search_l1(query).concat(this.searchService.search_l2(query)).sort((a, b) => a[0] - b[0])
-      const t1 = Date.now();
+      let t1 = Date.now();
       console.log(`Performed search of ${ENTRIES.length} entries in ${(t1-t0).toString()} ms`)
+      t0 = Date.now();
+      const linearResults = this.linearSearch(query);
+      t1 = Date.now();
+      console.log(linearResults)
+      console.log(`Performed linear search of ${ENTRIES.length} entries in ${(t1-t0).toString()} ms`)
       this.matches = results.filter((result) => result[0] < this.partialThreshold)
       this.partMatches = results.filter((result) => result[0] >= this.partialThreshold && result[0] < this.maybeThreshold )
       this.maybeMatches = results.filter((result) => result[0] >= this.maybeThreshold)
