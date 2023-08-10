9b04a5ab59b0895a284931d769c0bf801912b0b5
Aidan Pine <hello@aidanpine.ca>
2023-08-10 16:12:40 -0700

    fix(search): fix normalization function

--- packages/search/src/lib/search.spec.ts
+++ packages/search/src/lib/search.spec.ts
@@ -1,28 +1,47 @@
-import {  constructTransducer, defaultNormalization, englishStemmer } from './search';
-
+import {
+  constructTransducer,
+  create_normalization_function,
+  englishStemmer,
+} from './search';
 
 describe('stemmer', () => {
   it('should do basic stemming', () => {
-    expect(englishStemmer('relation')).toEqual('relate');
+    expect(englishStemmer('relation')).toEqual('relat');
   });
 });
 
 describe('normalize', () => {
   it('should lowercase', () => {
+    const defaultNormalization = create_normalization_function({
+      lower: true,
+      unicode_normalization: 'NFC',
+      replace_rules: [],
+      remove_punctuation: "[.,/#!$%^&?*';:{}=\\-_`~()]",
+    });
     expect(defaultNormalization('rElAtE')).toEqual('relate');
   });
 });
 
 describe('normalize', () => {
   it('should remove punctuation', () => {
+    const defaultNormalization = create_normalization_function({
+      lower: true,
+      unicode_normalization: 'NFC',
+      replace_rules: [],
+      remove_punctuation: "[.,/#!$%^&?*';:{}=\\-_`~()]",
+    });
     expect(defaultNormalization('rElAtE,;')).toEqual('relate');
   });
 });
 
 describe('transducer', () => {
-  const dummyTerms = ['dog', 'cat']
+  const dummyTerms = ['dog', 'cat'];
   it('should be able to search', () => {
-    expect(constructTransducer({terms: dummyTerms, include_distance: false}).transduce('kat', 1)).toContain('cat');
+    expect(
+      constructTransducer({
+        terms: dummyTerms,
+        include_distance: false,
+      }).transduce('kat', 1)
+    ).toContain('cat');
   });
 });
-
