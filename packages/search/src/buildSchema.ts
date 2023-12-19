42f987efb49123997e9e67780ce7b0069add75b4
Aidan Pine <hello@aidanpine.ca>
2023-12-18 17:07:00 -0800

    fix: build without sourcemap

--- packages/search/src/buildSchema.ts
+++ packages/search/src/buildSchema.ts
@@ -1,8 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const jsonSchema = require('json-schema-to-typescript')
+const jsonSchema = require('json-schema-to-typescript');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const fs = require('fs');
 
 // compile mtd from file
-jsonSchema.compileFromFile('../schemas/mtd.json')
-  .then((ts: any) => fs.writeFileSync('src/lib/mtd.d.ts', ts))
+jsonSchema
+  .compileFromFile('../schemas/mtd.json')
+  .then((ts: any) => fs.writeFileSync('src/lib/mtd.ts', ts));
