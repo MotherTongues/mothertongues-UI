604a673294e8a2955b8f179233399db2232a8373
David Huggins-Daines <dhd@ecolingui.ca>
2024-02-21 19:11:45 -0500

    feat: generate base app with nx

---
+++ packages/mtd-web-ui/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mothertongues-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'mtd-web-ui';
+}
