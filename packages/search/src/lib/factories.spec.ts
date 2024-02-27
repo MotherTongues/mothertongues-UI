0d0c31e657125f70aa9098693e69f6d4763444b0
PT Anderson <hello@lexicographyllc.com>
2024-02-27 15:56:04 -0800

    tests: add factories tests

--- packages/search/src/lib/factories.spec.ts
+++ packages/search/src/lib/factories.spec.ts
@@ -1,14 +1,29 @@
 import { constructTransducer } from './factories';
 
 describe('transducer', () => {
-    const dummyTerms = ['dog', 'cat'];
     it('should be able to search', () => {
+      const dummyTerms = ['dog', 'cat'];
+
       expect(
         constructTransducer({
           terms: dummyTerms,
           include_distance: false,
         }).transduce('kat', 1)
       ).toContain('cat');
+    });
+
+    it('should return only maximum number of candidates', () => {
+      const dummyTerms = ['rat', 'cat', 'bat', 'fat', 'mat', 'pat'];
+
+      const transducer = constructTransducer({
+        terms: dummyTerms,
+        include_distance: false,
+        maximum_candidates: 2
+      });
+
+      const result = transducer.transduce('kat', 1);
+
+      expect(result.length).toEqual(2);
     });
   });
   
