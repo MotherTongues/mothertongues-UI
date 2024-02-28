deeb58166c909fcb965dc3f5c1a0173bed21b3ce
PT Anderson <hello@lexicographyllc.com>
2024-02-28 14:19:31 -0800

    tests: more facory tests

--- packages/search/src/lib/factories.spec.ts
+++ packages/search/src/lib/factories.spec.ts
@@ -12,18 +12,39 @@ describe('transducer', () => {
       ).toContain('cat');
     });
 
-    it('should return only maximum number of candidates', () => {
-      const dummyTerms = ['rat', 'cat', 'bat', 'fat', 'mat', 'pat'];
+    it('should return only maximum number of candidates in alphabetical order', () => {
+      const dummyTerms = ['rat', 'cat', 'bat', 'mat', 'hat', 'pat'];
 
       const transducer = constructTransducer({
         terms: dummyTerms,
         include_distance: false,
-        maximum_candidates: 2
+        maximum_candidates: 3
       });
 
       const result = transducer.transduce('kat', 1);
 
-      expect(result.length).toEqual(2);
+      expect(result.length).toEqual(3);
+      expect(result[0]).toEqual('bat');
+      expect(result[1]).toEqual('cat');
+      expect(result[2]).toEqual('hat');
+    });
+
+    it('should return only maximum number of candidates in order they were given', () => {
+      const dummyTerms = ['rat', 'cat', 'bat', 'mat', 'hat', 'pat'];
+
+      const transducer = constructTransducer({
+        terms: dummyTerms,
+        include_distance: false,
+        maximum_candidates: 3,
+        sort_candidates: false
+      });
+
+      const result = transducer.transduce('kat', 1);
+
+      expect(result.length).toEqual(3);
+      expect(result[0]).toEqual('rat')
+      expect(result[1]).toEqual('cat')
+      expect(result[2]).toEqual('bat')
     });
   });
   
