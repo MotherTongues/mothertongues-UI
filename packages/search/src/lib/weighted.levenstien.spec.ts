c2462bcbc72c588de04184849d3258d00244465f
PT Anderson <hello@lexicographyllc.com>
2024-02-29 14:37:09 -0800

    tests: more weighted levenstien tests

--- packages/search/src/lib/weighted.levenstien.spec.ts
+++ packages/search/src/lib/weighted.levenstien.spec.ts
@@ -8,13 +8,66 @@ describe('DistanceCalculator', () => {
         expect(result).toBe(0);
         });
     
-        // Add more test cases for other methods and scenarios
-        // ...
-    
         it('should use default substitution cost when no specific cost is provided', () => {
         const calculator = new DistanceCalculator({});
         const result = calculator.getSubstitutionCost('a', 'b');
         expect(result).toBe(1.0); // Assuming defaultSubstitutionCost is 1.0
         });
-    })
-  });
+        
+        it('should use default substitution cost when no specific cost is provided', () => {
+            const calculator = new DistanceCalculator({
+                defaultSubstitutionCost: .5
+              });
+            const result = calculator.getSubstitutionCost('a', 'b');
+            expect(result).toBe(.5);
+        });
+    });
+
+    describe('getEditDistance', () => {
+        it('should return length if one input is empty string', () => {
+            const calculator = new DistanceCalculator({});
+
+            expect(calculator.getEditDistance('', 'happy')).toBe(5);
+            expect(calculator.getEditDistance('days', '')).toBe(4);
+
+        });
+
+        it('should calculate edit distance correctly', () => {
+          const calculator = new DistanceCalculator({
+            insertionCost: 1,
+            deletionCost: 1,
+            insertionAtBeginningCost: 0.5,
+            deletionAtEndCost: 0.5,
+            substitutionCosts: {},
+            defaultSubstitutionCost: 1,
+          });
+      
+          expect(calculator.getEditDistance('hello', 'world')).toBe(4);
+
+          expect(calculator.getEditDistance('sitting', 'kitten')).toBe(3);
+          expect(calculator.getEditDistance('kitten', 'sitting')).toBe(2.5);
+          
+          // TODO: Add more test cases 
+        });
+    });
+
+    // describe('getLeastEditDistance', () => {
+    //     it('should calculate least edit distance correctly', () => {
+    //       const calculator = new DistanceCalculator({
+    //         insertionCost: 1,
+    //         deletionCost: 1,
+    //         insertionAtBeginningCost: 0.5,
+    //         deletionAtEndCost: 0.5,
+    //         substitutionCosts: {},
+    //         defaultSubstitutionCost: 1,
+    //       });
+      
+    //       const bs1 = ['kitten', 'sitting'];
+    //       const bs2 = ['hello', 'world'];
+    //       expect(calculator.getLeastEditDistance('kitten', bs1)).toEqual(3);
+    //       expect(calculator.getLeastEditDistance('hello', bs2)).toEqual(4);
+          
+    //       // TODO: Add more test cases as needed
+    //     });
+    // });
+});
