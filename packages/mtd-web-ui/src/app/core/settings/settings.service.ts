f53a5a4e553903125ee9ce5bfa1b804bd1846cce
David Huggins-Daines <dhd@ecolingui.ca>
2024-02-29 14:30:54 -0800

    feat: save/restore settings from local storage

--- packages/mtd-web-ui/src/app/core/settings/settings.service.ts
+++ packages/mtd-web-ui/src/app/core/settings/settings.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { LocalStorageService } from '../core.module';
-import { AnimationsService } from "../animations/animations.service";
+import { AnimationsService } from '../animations/animations.service';
 
-const SETTINGS_KEY = "settings";
+const SETTINGS_KEY = 'settings';
 export interface SettingsState {
   language: string;
   theme: string;
@@ -14,55 +14,87 @@ export interface SettingsState {
 }
 
 const DEFAULT_STATE: SettingsState = {
-  language: "en",
-  theme: "default-theme",
+  language: 'en',
+  theme: 'default-theme',
   autoNightMode: false,
-  nightTheme: "black-theme",
+  nightTheme: 'black-theme',
   pageAnimations: true,
   elementsAnimations: true,
   hour: 12,
-}
+};
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SettingsService {
   public state: SettingsState;
 
-  constructor(private localStorage: LocalStorageService,
-              private animations: AnimationsService) {
+  constructor(
+    private localStorage: LocalStorageService,
+    private animations: AnimationsService
+  ) {
     const stored = this.localStorage.getItem(SETTINGS_KEY);
-    if (stored)
-      this.state = stored;
+    if (stored) this.state = stored;
     else {
       this.state = DEFAULT_STATE;
     }
-    this.animations.updateRouteAnimationType(this.state.pageAnimations,
-                                             this.state.elementsAnimations);
+    this.animations.updateRouteAnimationType(
+      this.state.pageAnimations,
+      this.state.elementsAnimations
+    );
   }
 
-  get language() { return this.state.language; }
-  set language(val: string) { this.state.language = val; }
+  saveState() {
+    this.localStorage.setItem(SETTINGS_KEY, this.state);
+  }
 
-  get theme() { return this.state.theme; }
-  set theme(val: string) { this.state.theme = val; }
+  get language() {
+    return this.state.language;
+  }
+  set language(val: string) {
+    this.state.language = val;
+    this.saveState();
+  }
 
-  get autoNightMode() { return this.state.autoNightMode; }
-  set autoNightMode(val: boolean) { this.state.autoNightMode = val; }
-  
-  get pageAnimations() { return this.state.pageAnimations; }
+  get theme() {
+    return this.state.theme;
+  }
+  set theme(val: string) {
+    this.state.theme = val;
+    this.saveState();
+  }
+
+  get autoNightMode() {
+    return this.state.autoNightMode;
+  }
+  set autoNightMode(val: boolean) {
+    this.state.autoNightMode = val;
+    this.saveState();
+  }
+
+  get pageAnimations() {
+    return this.state.pageAnimations;
+  }
   set pageAnimations(val: boolean) {
     this.state.pageAnimations = val;
     // FIXME: Use this very bogus API for the moment (should just rely
     // on change detection like everything else)
-    this.animations.updateRouteAnimationType(this.state.pageAnimations,
-                                             this.state.elementsAnimations);
+    this.animations.updateRouteAnimationType(
+      this.state.pageAnimations,
+      this.state.elementsAnimations
+    );
+    this.saveState();
   }
-  
-  get elementsAnimations() { return this.state.elementsAnimations; }
+
+  get elementsAnimations() {
+    return this.state.elementsAnimations;
+  }
   set elementsAnimations(val: boolean) {
     this.state.elementsAnimations = val;
-    this.animations.updateRouteAnimationType(this.state.pageAnimations,
-                                             this.state.elementsAnimations);
+    this.animations.updateRouteAnimationType(
+      this.state.pageAnimations,
+      this.state.elementsAnimations
+    );
+    this.saveState();
   }
 }
