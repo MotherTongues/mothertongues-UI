8d3b352d8007a5b9251e1c1f70cddb6c9c69f4f3
PT Anderson <hello@lexicographyllc.com>
2024-03-04 14:17:49 -0800

    tests: weighted levenstien again

--- packages/search/src/lib/weighted.levenstien.spec.ts
+++ packages/search/src/lib/weighted.levenstien.spec.ts
@@ -32,23 +32,53 @@ describe('DistanceCalculator', () => {
 
         });
 
-        it('should calculate edit distance correctly', () => {
-          const calculator = new DistanceCalculator({
-            insertionCost: 1,
-            deletionCost: 1,
-            insertionAtBeginningCost: 0.5,
-            deletionAtEndCost: 0.5,
-            substitutionCosts: {},
-            defaultSubstitutionCost: 1,
-          });
+        it('should calculate edit distance with defaults correctly', () => {
+          const calculator = new DistanceCalculator({});
       
           expect(calculator.getEditDistance('hello', 'world')).toBe(4);
 
           expect(calculator.getEditDistance('sitting', 'kitten')).toBe(3);
           expect(calculator.getEditDistance('kitten', 'sitting')).toBe(2.5);
-          
-          // TODO: Add more test cases 
         });
+
+        it('should calculate edit distance with custom sub values correctly', () => {
+            const calculator = new DistanceCalculator({
+                insertionCost: .25,
+                deletionCost: .8,
+                insertionAtBeginningCost: 0.75,
+                deletionAtEndCost: 0.6,
+                substitutionCosts: {},
+                defaultSubstitutionCost: .45,
+            });
+        
+            // insertion
+            expect(calculator.getEditDistance('hat', 'that')).toBe(.75);
+            expect(calculator.getEditDistance('cart', 'cat')).toBe(.25);
+
+            // deletion
+            expect(calculator.getEditDistance('hat', 'hats')).toBe(.6);
+            expect(calculator.getEditDistance('cat', 'cart')).toBe(.8);
+
+            // substitution
+            expect(calculator.getEditDistance('bat', 'bet')).toBe(.45);
+        });
+
+        it('should create calculator that favors affixation', () => {
+            const calculator_withEndDeletion_lowValue = new DistanceCalculator({
+                deletionAtEndCost: 0.1
+            });
+
+            const v1 = calculator_withEndDeletion_lowValue.getEditDistance('happy', 'happiness');
+            expect(calculator_withEndDeletion_lowValue.getEditDistance('happy', 'poppy')).toBe(2);
+
+            const calculator_defaultValues = new DistanceCalculator({});
+            const v2 = calculator_defaultValues.getEditDistance('happy', 'happiness');
+
+            expect(v1).toBeLessThan(v2);
+
+        });
+
+
     });
 
     // describe('getLeastEditDistance', () => {
@@ -64,8 +94,8 @@ describe('DistanceCalculator', () => {
       
     //       const bs1 = ['kitten', 'sitting'];
     //       const bs2 = ['hello', 'world'];
-    //       expect(calculator.getLeastEditDistance('kitten', bs1)).toEqual(3);
-    //       expect(calculator.getLeastEditDistance('hello', bs2)).toEqual(4);
+    //       expect(calculator.getLeastEditDistance('kitten', bs1)).toBe(2.5);
+    //       expect(calculator.getLeastEditDistance('hello', bs2)).toBe(4);
           
     //       // TODO: Add more test cases as needed
     //     });
