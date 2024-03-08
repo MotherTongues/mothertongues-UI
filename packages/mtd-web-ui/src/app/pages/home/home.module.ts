6a2c56f8ff620c68f263002d3bef0c141d15633b
David Huggins-Daines <dhd@ecolingui.ca>
2024-03-08 14:02:26 -0500

    fix: make the search link work again (oops)

--- packages/mtd-web-ui/src/app/pages/home/home.module.ts
+++ packages/mtd-web-ui/src/app/pages/home/home.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../../shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, SharedModule]
+  imports: [CommonModule, SharedModule, RouterModule]
 })
 export class HomeModule {}
