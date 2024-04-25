4fc8dcfe1b6c85c57f7cec1b4eaa7e464b8c2b06
David Huggins-Daines <dhd@ecolingui.ca>
2024-04-25 11:07:52 -0400

    feat: add environment for use in CLI

---
+++ packages/mtd-web-ui/src/environments/environment.py.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  appName: 'MotherTongues Dictionary',
+  envName: 'pydev',
+  production: false,
+  test: false,
+  useHash: true,
+  dataPath: './assets/dictionary_data.json',
+  versions: {
+    app: "0.0.0"
+  }
+};
